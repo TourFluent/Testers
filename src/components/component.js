8d612b85e6eb0fa80a9c8497124ef8dc58ef300c
anisahmw <109682880+anisahmw@users.noreply.github.com>
2022-10-09 22:47:23 +0530

    Delete src directory

--- src/components/component.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import './component.css'
-
-const AppComponent = (props) => {
-  return <div id="header" className="component-container"></div>
-}
-
-export default AppComponent
